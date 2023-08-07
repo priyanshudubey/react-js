ef04180f05177baedad080ae17c64855e5aa2703
priyanshudubey <priyanshu0dubey@gmail.com>
2023-08-07 22:02:06 +0530

    playing with parcel

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client" ;
+
 const heading = React.createElement("div", 
     {id:"Parent"}, 
     React.createElement("div", {id:"Child"},[
     React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag")])
+    React.createElement("h2", {}, "I am an h2 tag")])   
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
