6005e61aa00e6fb64d76c4472928161d2dd0cb42
priyanshudubey <priyanshu0dubey@gmail.com>
2023-08-09 22:01:41 +0530

    minor change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client" ;
 
 const heading = React.createElement("div", 
-    {id:"Parent"}, 
+    {id:"ParentChild"}, 
     React.createElement("div", {id:"Child"},[
     React.createElement("h1", {}, "I am an h1 tag"),
     React.createElement("h2", {}, "I am an h2 tag")])   
